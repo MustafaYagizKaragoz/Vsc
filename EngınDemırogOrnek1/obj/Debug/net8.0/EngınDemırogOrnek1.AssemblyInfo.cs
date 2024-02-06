859902586d06c8fd38cba0b93aa8be5a2b98e4e2
Yağız Karagöz <ygz7003@outlook.com>
2024-02-07 02:05:51 +0300

    yeni

--- EngınDemırogOrnek1/obj/Debug/net8.0/EngınDemırogOrnek1.AssemblyInfo.cs
+++ EngınDemırogOrnek1/obj/Debug/net8.0/EngınDemırogOrnek1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EngınDemırogOrnek1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa590494055ef4b4bbac90f4c19c696cd1ad921")]
 [assembly: System.Reflection.AssemblyProductAttribute("EngınDemırogOrnek1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EngınDemırogOrnek1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
